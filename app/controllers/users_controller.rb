9a7dd10c4af3952af10d3da64e84f3b01f33fb7e
Dallie Hill <lenore776@gmail.com>
2015-08-09 22:22:29 -0400

    More stuff with users and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,30 @@ class UsersController < ApplicationController
 
     def show
       @user = User.find(params[:id])
-      @post = Post.new
+    end
+
+    def follow
+      @user = User.find(params[:id])
+      current_user.follow(@user)
+      redirect_to :back
+    end
+
+    def unfollow
+      @user = User.find(params[:id])
+      current_user.stop_following(@user)
+      redirect_to :back
+    end
+
+    def block
+      @user = User.find(params[:id])
+      current_user.block(@user)
+      redirect_to :back
+    end
+
+    def unblock
+      @user = User.find(params[:id])
+      current_user.unblock(@user)
+      redirect_to :back
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   resources :users
   resources :posts
 
+  resources :users do
+    member do
+      get :follow
+      get :unfollow
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
