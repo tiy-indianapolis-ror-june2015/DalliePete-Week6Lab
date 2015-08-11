9a3f84c457813ad21dcee965eac7e3cda207fef5
esttres <esttres@hotmail.com>
2015-08-10 22:37:12 -0400

    Several changes to get Wicked to work.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: "users/registrations"}
+  devise_for :users, controllers: {:registrations => "users/registrations"}
   resources :posts
 
   resources :users do
