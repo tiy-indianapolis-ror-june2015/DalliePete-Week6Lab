273e1f52e14f13b4288020bd17716329378a4176
Dallie Hill <lenore776@gmail.com>
2015-08-11 00:10:38 -0400

    Removed create from user controller as causing issues with wicked fixed view for profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,20 +8,6 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create
-    @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
     def show
       @user = User.find(params[:id])
     end
